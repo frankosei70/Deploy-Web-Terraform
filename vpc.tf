727a11aeff78760be56611388fd05473b49abf0c
Frank Osei wusu <frankosei70@yahoo.com>
2024-12-06 12:37:04 -0500

    created variable file

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+# Creating VPC
+resource "aws_vpc" "vpc" {
+  cidr_block              = var.vpc_cidr
+  instance_tenancy        = "default"
+  enable_dns_hostnames    = true
+
+  tags = {
+    Name = "dev vpc"
+  }
+}
